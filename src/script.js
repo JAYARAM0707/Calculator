e13bcb408ad5408811486a2f2e05445679d2a58e
Jayaram <marnijayaram0707@gmail.com>
2025-02-07 20:30:40 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,30 @@
+function main()
+{
+    let r1=calc(a,b,type);
+    console.log("The result is : " );
+}
+function calc(a,b,type)
+{
+    var res=0;
+}
+function sum(a,b)
+{
+    return a+b;
+}
+function diff(a,b)
+{
+    return a-b;
+}
+function mul(a,b)
+{
+    return a*b;
+}
+function div(a,b)
+{
+    return a/b;
+}
+function mod(a,b)
+{
+    return a%b;
+}
+main();
